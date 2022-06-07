eb0fb377e95b0019368a7dbaba966e6be6e04451
Zain Ullah <22411645+ZainAchak@users.noreply.github.com>
2022-06-07 21:42:25 +0200

    commit

---
+++ 3. Dataset and data loader/1. Data loaders.py
@@ -0,0 +1,15 @@
+import torch
+import torchvision
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+import math
+
+class Winedataset(Dataset):
+    def __init__(self):
+        super().__init__()
+
+    def __getitem__(self, index):
+        pass
+
+    def __len__(self):
+        pass
